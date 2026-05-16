cf1fae6faf0757ccc3e1d4d8ac69789259e6ff21
arunkumar.tn <arunkumar.tn@ARUNKUMAR-WS>
2026-05-16 15:13:36 +0530

    Added dictionaries

--- SampleProject/ShellProject/ViewModels/ShellProjectViewModel.cs
+++ SampleProject/ShellProject/ViewModels/ShellProjectViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ShellProject.ViewModels
 {
@@ -15,6 +16,7 @@ namespace ShellProject.ViewModels
         public ShellProjectViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+            InitializeTheme("Cyan");
             Console.WriteLine("Constructor called in Shell Project");
         }
         public bool IsNavigationTarget(NavigationContext navigationContext) => true;
@@ -24,7 +26,28 @@ namespace ShellProject.ViewModels
         {
            
         }
-
+        private void InitializeTheme(string theme)
+        {
+            var dictionaryPath = GetThemeDictionaryPath(theme);
+            var resourceDictionary = new ResourceDictionary
+            { Source = new Uri(dictionaryPath, UriKind.RelativeOrAbsolute) };
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+        public string GetThemeDictionaryPath(string themeName)
+        {
+            string result = null;
+            if (string.Equals(themeName, "Cyan"))
+            {
+                result = @"pack://application:,,,/MaterialDesign;Component/Themes/DictionaryMergedCyan.xaml";
+            }
+            else if (string.Equals(themeName, "Blue"))
+            {
+                result = @"pack://application:,,,/MaterialDesign;Component/Themes/DictionaryMergedBlue.xaml";
+            }
+            
+            return result;
+        }
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
             Console.WriteLine("OnNavigated called in Shell Project");
